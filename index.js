d4574a5f00009a1a65c0f0e5d52e13977f5555ff
monciasty <117080659+monciasty@users.noreply.github.com>
2024-08-20 19:44:32 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+    function onSyneriseLoad() {
+        SR.init({
+            'trackerKey':'57ae98e4-d5f5-4b57-85fd-526337fba4fd',
+        });
+    }
+
+    (function(s,y,n,e,r,i,se){s['SyneriseObjectNamespace']=r;s[r]=s[r]||[],
+     s[r]._t=1*new Date(),s[r]._i=0,s[r]._l=i;var z=y.createElement(n),
+     se=y.getElementsByTagName(n)[0];z.async=1;z.src=e;se.parentNode.insertBefore(z,se);
+     z.onload=z.onreadystatechange=function(){var rdy=z.readyState;
+     if(!rdy||/complete|loaded/.test(z.readyState)){s[i]();z.onload = null;
+     z.onreadystatechange=null;}};})(window,document,'script',
+     '//web.snrbox.com/synerise-javascript-sdk.min.js','SR', 'onSyneriseLoad');
